9242458729d79c1bff9fa69f7e49075038d15fac
Mohamed Esmail <dev.mohamed.esmail@gmail.com>
2026-02-05 10:40:24 +0200

    configure files of project

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+import About from "@/components/sections/about";
+import Projects from "@/components/sections/projects";
 import { Button } from "@/components/ui/button"
+import Footer from "@/components/ui/footer";
+import Navbar from "@/components/ui/navbar";
 
 export default function Home() {
+  
   return (
-    <>
-      <h1>Hello</h1>
-      <Button>Click me</Button>
-    </>
+    <div>
+      <Navbar />
+      <About />
+      <Projects />
+      <Footer />
+    </div>
   );
 }
